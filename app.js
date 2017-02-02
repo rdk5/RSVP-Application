2873944d508731d82f43085cc2420021fa95ebe1
Ricky Kharawala <rdk5@njit.edu>
2017-02-01 22:18:38 -0500

    Remove RSVP
    -Added a remove button and logic

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
+function createLI(text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
+  const button = document.createElement('button');
+  button.textContent = 'remove';
+  li.appendChild(button);
+  return li;
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = input.value;
     input.value = '';
-    const li = document.createElement('li');
-    li.textContent = text;
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    label.appendChild(checkbox);
-    li.appendChild(label);
+    const li = createLI(text);
     ul.appendChild(li);
 });
 
@@ -28,3 +36,11 @@ ul.addEventListener('change', (e) => {
     listItem.className = '';
   }
 });
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
+  }
+})
